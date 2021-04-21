8fa994acc9d1db3e979bb83f8bd02b0ea90902fc
Sean Dougherty <sean@piworld.co.uk>
2021-04-21 09:50:41 +0100

    first commit

---
+++ dist/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log('hello');
+console.log('lol');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9hbm90aGVyLy4vc3JjL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQSIsImZpbGUiOiJidW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaGVsbG8nKTtcclxuY29uc29sZS5sb2coJ2xvbCcpO1xyXG4iXSwic291cmNlUm9vdCI6IiJ9
