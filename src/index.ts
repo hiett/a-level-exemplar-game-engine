93abcf30760f5c06c69dccf2d77417b180c96185
seandougherty1 <15c16331@marling.school>
2021-04-28 10:01:37 +0100

    rectangles n that

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash'; 
 import { Application, Loader, LoaderResource, Sprite } from 'pixi.js';
-;
+import * as PIXI from "pixi.js";
 
 import './style.css'; 
 
@@ -21,7 +21,31 @@ function component(){
 
     document.body.appendChild(app.view);
     return element;
+
+    app.loader.load(setup)
+
+    let rect: Sprite
+
+    function setup(){
+        rect = new PIXI.Rectangle(100, 150, 50, 50);
+
+        rect.position.set(50, 60);
+        rect.rotation = Math.PI / 3;
+        rect.anchor.set(0.5, 0.5);
+
+        //Add the cat to the stage
+        app.stage.addChild(rect);
+
+        app.ticker.add(d => gameLoop(d))
+    }
+
+    function gameLoop(delta: number) {
+        rect.rotation += 0.01;
+    }  
 }
+
+    
+
 
 document.body.appendChild(component());
 
