d267ac4dbc24a623a418aea911cc2f46b0007f15
seandougherty1 <15c16331@marling.school>
2021-04-28 09:41:00 +0100

    canvas drawn

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-console.log('hello');
+import * as _ from 'lodash'; 
+import { Application, Loader, LoaderResource, Sprite } from 'pixi.js';
+;
 
-console.log('lol')
+import './style.css'; 
+
+
+function component(){
+    const element = document.createElement('div'); 
+
+    element.innerHTML = _.join('Hello this is a test')
+
+
+    let app = new Application({width: 265, height: 256});
+    app.renderer.backgroundColor= 0x061639;
+    
+    app.renderer.view.style.position = 'absolute';
+    app.renderer.view.style.display = "block";
+
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(app.view);
+    return element;
+}
+
+document.body.appendChild(component());
+
+
