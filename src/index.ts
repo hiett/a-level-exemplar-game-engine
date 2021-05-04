f6137f3ce834c730a756abf7dd484a85112a5167
Sean Dougherty <15c16331@marling.school>
2021-05-04 14:35:37 +0100

    man has drawn a square

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,42 @@
-import * as _ from 'lodash'; 
-import { Application, Loader, LoaderResource, Sprite } from 'pixi.js';
+import * as _ from 'lodash';
+import { Application, Loader, LoaderResource, Rectangle, Sprite } from 'pixi.js';
 import * as PIXI from "pixi.js";
 
-import './style.css'; 
+
+import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join('Hello this is a test')
 
 
-function component(){
-    const element = document.createElement('div'); 
+  let app = new Application({ width: 512, height: 512 });
+  app.renderer.backgroundColor = 0x061639;
 
-    element.innerHTML = _.join('Hello this is a test')
+  app.renderer.view.style.position = 'absolute';
+  app.renderer.view.style.display = "block";
+
+  document.body.appendChild(app.view);
 
 
-    let app = new Application({width: 265, height: 256});
-    app.renderer.backgroundColor= 0x061639;
-    
-    app.renderer.view.style.position = 'absolute';
-    app.renderer.view.style.display = "block";
+  let rect: Sprite;
 
-    app.renderer.resize(window.innerWidth, window.innerHeight);
+  var graphics = new PIXI.Graphics();
 
-    document.body.appendChild(app.view);
-    return element;
+  graphics.beginFill(0xFF0000);
 
-    app.loader.load(setup)
+  // set the line style to have a width of 5 and set the color to red
+  graphics.lineStyle(5, 0xFF0000);
 
-    let rect: Sprite
+  // draw a rectangle
+  graphics.drawRect(0, 0, 100, 100);
 
-    function setup(){
-        rect = new PIXI.Rectangle(100, 150, 50, 50);
+  app.stage.addChild(graphics);
+  return element;
 
-        rect.position.set(50, 60);
-        rect.rotation = Math.PI / 3;
-        rect.anchor.set(0.5, 0.5);
 
-        //Add the cat to the stage
-        app.stage.addChild(rect);
-
-        app.ticker.add(d => gameLoop(d))
-    }
-
-    function gameLoop(delta: number) {
-        rect.rotation += 0.01;
-    }  
 }
-
-    
-
-
 document.body.appendChild(component());
 
 
