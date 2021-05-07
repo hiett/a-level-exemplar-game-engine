c4e4ec3204abf1ea0264be511ca764a17b9f9496
Sean Dougherty <15c16331@marling.school>
2021-05-07 11:46:22 +0100

    sprite working

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as _ from 'lodash';
-import { Application, Loader, LoaderResource, Rectangle, Sprite } from 'pixi.js';
+import { Application, Loader, LoaderResource, PlaneGeometry, Rectangle, Sprite } from 'pixi.js';
 import * as PIXI from "pixi.js";
 
 
 import './style.css';
+
 
 function component() {
   const element = document.createElement('div');
@@ -19,11 +20,65 @@ function component() {
 
   document.body.appendChild(app.view);
 
+  //Introduces cube sprite
 
-  let sprite = PIXI.Sprite.from('./images/square.png');
+  let player: Sprite = PIXI.Sprite.from("./images/square.png")
+  player.anchor.set(0.5);
+  player.x = app.view.width /2;
+  player.y = app.view.height /2;
+
+  app.stage.addChild(player);
+
+  //key event handleres
+  function keyboard(value: any) {
+    let key: any = {};
+    key.value = value;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    //The `downHandler`
+    key.downHandler = (event: { key: any; preventDefault: () => void; }) => {
+      if (event.key === key.value) {
+        if (key.isUp && key.press) key.press();
+        key.isDown = true;
+        key.isUp = false;
+        event.preventDefault();
+      }
+    };
+  
+    //The `upHandler`
+    key.upHandler = (event: { key: any; preventDefault: () => void; }) => {
+      if (event.key === key.value) {
+        if (key.isDown && key.release) key.release();
+        key.isDown = false;
+        key.isUp = true;
+        event.preventDefault();
+      }
+    };
+  
+    //Attach event listeners
+    const downListener = key.downHandler.bind(key);
+    const upListener = key.upHandler.bind(key);
+    
+    window.addEventListener(
+      "keydown", downListener, false
+    );
+    window.addEventListener(
+      "keyup", upListener, false
+    );
+    
+    // Detach event listeners
+    key.unsubscribe = () => {
+      window.removeEventListener("keydown", downListener);
+      window.removeEventListener("keyup", upListener);
+    };
+    
+    return key;
+  }
+
 
   return element;
-
 
 
 }
