7b35fb01673d3561a1aa7defe7bace5a5ef604e7
seandougherty1 <15c16331@marling.school>
2021-05-17 22:23:13 +0100

    keyevents working

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,55 @@
 import * as _ from 'lodash';
-import { Application, Loader, LoaderResource, PlaneGeometry, Rectangle, Sprite, } from 'pixi.js';
+import { Application, Loader, LoaderResource, PlaneGeometry, Rectangle, Sprite } from 'pixi.js';
 import * as PIXI from "pixi.js";
-import 'pixi-keyboard';
+
+let keys:any = {};
+
+import './style.css';
 
 
 function component() {
-
-  let app: Application;
-  let player: Sprite;
-
   const element = document.createElement('div');
 
-  element.innerHTML = _.join('Hello this is a test')
-
-
-  app = new Application({
-    width: 512,
-    height: 512,
-    backgroundColor: 0xAAAAAA,
-  });
+//Creating the application/stage in PIXI
+  let app = new Application({ width: 512, height: 512 });
+  app.renderer.backgroundColor = 0x061639;
 
   app.renderer.view.style.position = 'absolute';
   app.renderer.view.style.display = "block";
+
   document.body.appendChild(app.view);
 
-  //Introduces cube sprite
+  //Introduces simple cube sprite from file. 
 
-  player = PIXI.Sprite.from("./images/square.png");
+  let player: Sprite = PIXI.Sprite.from("./images/square.png")
   player.anchor.set(0.5);
-  player.x = app.view.width / 2;
-  player.y = app.view.height / 2;
+  player.x = app.view.width /2;
+  player.y = app.view.height /2;
 
-  app.stage.addChild(player);
+  app.stage.addChild(player); //adds sprite to the application
 
-  
+  window.addEventListener("keydown",keysDown)
+  window.addEventListener("keyup", keysUp)
 
+  app.ticker.add(gameloop); 
 
-
-
-    return element;
+  return element;
 }
+
+function keysDown(e:any){
+  console.log(e.keyCode)
+  keys[e.keyCode] = true;
+}
+
+function keysUp(e:any){
+  console.log(e.keyCode)
+  keys[e.keyCode] = false;
+}
+
+function gameloop(){
+  
+}
+
+
+
 document.body.appendChild(component());
